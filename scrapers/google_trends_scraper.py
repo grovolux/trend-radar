8146512691815eddf37828390721ff473d7b18ab
grovolux <grovolux@gmail.com>
2026-03-12 16:07:53 +0100

    Update google_trends_scraper.py

--- scrapers/google_trends_scraper.py
+++ scrapers/google_trends_scraper.py
@@ -1,1 +1,10 @@
+from pytrends.request import TrendReq
+import pandas as pd
 
+pytrends = TrendReq()
+
+trending_searches = pytrends.trending_searches()
+
+df = pd.DataFrame(trending_searches)
+
+print(df)
