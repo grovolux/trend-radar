b5c4573ae4ef33707935c676fe26ef87172bb5a6
grovolux <grovolux@gmail.com>
2026-03-13 07:22:14 +0100

    Update google_trends_scraper.py

--- scrapers/google_trends_scraper.py
+++ scrapers/google_trends_scraper.py
@@ -1,10 +1,10 @@
 from pytrends.request import TrendReq
 import pandas as pd
 
-pytrends = TrendReq(hl='en-US', tz=360)
+pytrends = TrendReq()
 
-trending_searches = pytrends.trending_searches(pn='united_states')
+trending = pytrends.daily_trending_searches(pn='US')
 
-df = pd.DataFrame(trending_searches)
+df = pd.DataFrame(trending)
 
 print(df.to_string())
